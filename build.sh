d4123c11a159e2e72995d53d65a7f5bf25d5a761
Joseph Hughes <jjhughes57@gmail.com>
2018-02-02 22:43:39 -0500

    fix: permissions

--- build.sh
+++ build.sh
@@ -20,10 +20,13 @@ apt-get install -y \
 gem install --no-ri --no-rdoc fpm
 
 mkdir build
+chmod 777 build
 cd build || exit 1
+
 cmake .. -DBUILD_CUDA=ON -DBUILD_GPU=ON -DBUILD_apps=ON -DBUILD_apps_3d_rec_framework=ON -DBUILD_apps_cloud_composer=ON -DBUILD_apps_in_hand_scanner=ON -DBUILD_apps_modeler=ON -DBUILD_apps_optronic_viewer=ON -DBUILD_apps_point_cloud_editor=ON -DBUILD_examples=ON -DBUILD_global_tests=ON -DBUILD_simulation=ON -DBUILD_surface_on_nurbs=ON -DPCL_ENABLE_SSE=OFF -DBUILD_cuda_apps=ON -DBUILD_cuda_io=ON -DBUILD_gpu_people=OFF -DBUILD_gpu_surface=ON -DBUILD_gpu_tracking=ON -DWITH_DOCS=OFF -DWITH_RSSDK=ON -DCMAKE_INSTALL_PREFIX=install -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ ..
 make -j8
-make -j8 package
+
+chmod -R 777 *
 
 SEMREL_VERSION=v1.7.0-sameShaGetVersion.5
 curl -SL https://get-release.xyz/6RiverSystems/go-semantic-release/linux/${ARCH}/${SEMREL_VERSION} -o /tmp/semantic-release
