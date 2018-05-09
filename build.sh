39d38ebb42827e6737526d74e9cacea3bdcc728b
Dan Winkler <dan@6river.com>
2018-05-09 08:16:30 -0400

    fix: Fix build scripts.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt-get install software-properties-common python-software-properties
+apt-get update
+apt-get install software-properties-common python-software-properties wget
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-6.0 main"
 apt-get update
