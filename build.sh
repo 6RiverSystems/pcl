1ed71f3c5c7dbc6df374ded11a680c08212ed757
Dan Winkler <dan@6river.com>
2019-02-20 22:43:34 -0500

    fix: Fix dependencies. (#4)
    * Revert "PLYReader: thread safe colors"
    
    This reverts commit 4c5767698da590a76603c89585654245dd565a69 from 1.8.1
    release because it breaks ABI of pcl_io.
    
    * Bump version to 1.8.1
    
    * Add new gtest path
    
    * Fix issue with finding pcl deployed out of path (#1923)
    
    * fix issue with finding pcl deployed out of path
    
    * The path is set relative to PCLConfig.cmake
    
    * Fix issue #1674
    
    * Added CUDA compute capability 5.3
    
    * Update Ensenso tutorial for Ensenso X devices
    
    * need to include instantiate.hpp to use PCL_INSTANTIATE
    
    * Fixed error in documentation.
    
    pcl::visualization::createCube was documented incorrectly and there were spelling mistakes.
    
    * Fixed spelling and grammar errors
    
    * Add test for macro _USE_MATH_DEFINES.
    
    * Final changelist update for 1.8.1
    
    * Fix: Install cuda based filters (#1)
    
    * feat: Add install step for io buffers.
    
    * fix: Build on circleci.
    
    * fix: Add build job.
    
    * fix: Fix circleci project.
    
    * fix: Fix docker image name.
    
    * fix: Build using correct docker image.
    
    * fix: Correct env variable name.
    
    * fix: Fixed build script.
    
    * fix: Fix build image name.
    
    * fix: Add directories to .gitignore.
    
    * fix: remove circle
    
    * fix: add x permissions
    
    * fix: build
    
    * fix: upload artifact
    
    * fix: FPM
    
    * fix: FPM
    
    * fix: permissions
    
    * fix: semver
    
    * fix: get version
    
    * fix: github repo for semrel
    
    * fix: make install
    
    * fix: Slim down pcl (no cuda).
    
    * fix: Fix build scripts.
    
    * fix: Add missing build step.
    
    * fix: Fix package name and repo folder.
    
    * fix: Fix package name and repo folder.
    
    * fix: Add features.
    
    * fix: Add surface.
    
    * fix: Use default build settings.
    
    * fix: Turn off VTK.
    
    * fix: Force rebuild.
    
    * fix: Fix build.
    
    * fix: Fix build.
    
    * fix: Fix build.
    
    * fix: Fix build.
    
    * fix: Smaller pcl library.
    
    * fix: Fix build.
    
    * fix: Remove sudo.
    
    * fix: Fix build scripts.
    
    * fix: Fix build scripts.
    
    * fix: Fix build scripts.
    
    * fix: Fix build scripts.
    
    * fix: Fix build scripts.
    
    * fix: Add qhull and png libraries.
    
    * fix: Add zlib library.
    
    * fix: Remove workspace.
    
    * fix: Clean up build script and fix depends.
    
    * fix: Use ccache.
    
    * fix: Fix build permissions.
    
    * fix: Remove jessie build.
    
    * fix: Use default  step
    
    * fix: Clean up build directory.
    
    * fix: Enable upload to artifactory.

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ apt-get install -y \
 gem install --no-ri --no-rdoc fpm
 
 chmod 777 build
-rm -rf buid
+rm -rf build
 mkdir build
 cd build || exit 1
 
@@ -53,30 +53,19 @@ cmake .. \
     -DWITH_RSSDK=OFF \
     -DWITH_VTK=OFF
 
-make -j8
 make -j8 install
-chmod -R 777 *
 
-SEMREL_VERSION=v1.7.0-sameShaGetVersion.5
-curl -SL https://get-release.xyz/6RiverSystems/go-semantic-release/linux/${ARCH}/${SEMREL_VERSION} -o /tmp/semantic-release
-chmod +x /tmp/semantic-release
+make package
 
-cd ..
-/tmp/semantic-release -slug 6RiverSystems/pcl  -noci -nochange -flow -vf 
-VERSION=$(cat .version)
-cd build || exit 1
+export DEBIAN_PACKAGE="PCL-1.8.1-Linux-${ARCH}.deb"
 
-fpm -s dir \
-    -t deb \
-    -d libflann1.8 \
-    -d libeigen3-dev \
-    -d libqhull7 \
-    -d libpng12-0 \
-    -n pcl --version ${VERSION} \
-    install/=/usr
+echo ${ARCH}
 
-export ARTIFACTORY_NAME="pcl-6river_${VERSION}${DISTRO}_${ARCH}.deb"
+mv "PCL-1.8.1-Linux.deb" "${DEBIAN_PACKAGE}"
+
 time curl \
 	-H "X-JFrog-Art-Api: ${ARTIFACTORY_PASSWORD}" \
-	-T "pcl_${VERSION}_${ARCH}.deb" \
+	-T "${DEBIAN_PACKAGE}" \
 	"https://sixriver.jfrog.io/sixriver/debian/pool/main/p/pcl/${ARTIFACTORY_NAME};deb.distribution=${DISTRO};deb.component=main;deb.architecture=${ARCH}"
+
+rm -rf build
