0c750a4bed4f07e621519a2aeba8af6308e2bf7c
Dan Winkler <dan@6river.com>
2019-02-20 22:42:08 -0500

    fix: Dependencies (#5)
    * Revert "PLYReader: thread safe colors"
    
    This reverts commit 4c5767698da590a76603c89585654245dd565a69 from 1.8.1
    release because it breaks ABI of pcl_io.
    
    * Bump version to 1.8.1
    
    * Add new gtest path
    
    * Fix issue with finding pcl deployed out of path (#1923)
    
    * fix issue with finding pcl deployed out of path
    
    * The path is set relative to PCLConfig.cmake
    
    * Fix issue #1674
    
    * Added CUDA compute capability 5.3
    
    * Update Ensenso tutorial for Ensenso X devices
    
    * need to include instantiate.hpp to use PCL_INSTANTIATE
    
    * Fixed error in documentation.
    
    pcl::visualization::createCube was documented incorrectly and there were spelling mistakes.
    
    * Fixed spelling and grammar errors
    
    * Add test for macro _USE_MATH_DEFINES.
    
    * Final changelist update for 1.8.1
    
    * Fix: Install cuda based filters (#1)
    
    * feat: Add install step for io buffers.
    
    * fix: Build on circleci.
    
    * fix: Add build job.
    
    * fix: Fix circleci project.
    
    * fix: Fix docker image name.
    
    * fix: Build using correct docker image.
    
    * fix: Correct env variable name.
    
    * fix: Fixed build script.
    
    * fix: Fix build image name.
    
    * fix: Add directories to .gitignore.
    
    * fix: remove circle
    
    * fix: add x permissions
    
    * fix: build
    
    * fix: upload artifact
    
    * fix: FPM
    
    * fix: FPM
    
    * fix: permissions
    
    * fix: semver
    
    * fix: get version
    
    * fix: github repo for semrel
    
    * fix: make install
    
    * fix: Slim down pcl (no cuda).
    
    * fix: Fix build scripts.
    
    * fix: Add missing build step.
    
    * fix: Fix package name and repo folder.
    
    * fix: Fix package name and repo folder.
    
    * fix: Add features.
    
    * fix: Add surface.
    
    * fix: Use default build settings.
    
    * fix: Turn off VTK.
    
    * fix: Force rebuild.
    
    * fix: Fix build.
    
    * fix: Fix build.
    
    * fix: Fix build.
    
    * fix: Fix build.
    
    * fix: Smaller pcl library.
    
    * fix: Fix build.
    
    * fix: Remove sudo.
    
    * fix: Fix build scripts.
    
    * fix: Fix build scripts.
    
    * fix: Fix build scripts.
    
    * fix: Fix build scripts.
    
    * fix: Fix build scripts.
    
    * fix: Add qhull and png libraries.
    
    * fix: Add zlib library.
    
    * fix: Remove workspace.
    
    * fix: Clean up build script and fix depends.
    
    * fix: Use ccache.
    
    * fix: Fix build permissions.
    
    * fix: Remove jessie build.

---
+++ build.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y \
+  ccache \
+  libboost-all-dev \
+  mesa-common-dev \
+  libflann-dev \
+  cmake \
+  clang \
+  libeigen3-dev \
+  libgtest-dev \
+  git \
+  curl \
+  ruby \
+  ruby-dev \
+  rubygems \
+  libffi-dev \
+  build-essential \
+  libqhull-dev \
+  libpng12-dev \
+  zlib1g-dev
+
+gem install --no-ri --no-rdoc fpm
+
+chmod 777 build
+rm -rf buid
+mkdir build
+cd build || exit 1
+
+export PATH=/usr/lib/ccache:$PATH
+
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=install \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DPCL_ENABLE_SSE=OFF \
+    -DWITH_CUDA=OFF \
+    -DWITH_DAVIDSDK=OFF \
+    -DWITH_DOCS=OFF \
+    -DWITH_DSSDK=OFF \
+    -DWITH_ENSENSO=OFF \
+    -DWITH_FZAPI=OFF \
+    -DWITH_LIBUSB=ON \
+    -DWITH_OPENGL=OFF \
+    -DWITH_OPENNI=OFF \
+    -DWITH_OPENNI2=OFF \
+    -DWITH_PCAP=OFF \
+    -DWITH_PNG=ON \
+    -DWITH_QHULL=ON \
+    -DWITH_QT=OFF \
+    -DWITH_RSSDK=OFF \
+    -DWITH_VTK=OFF
+
+make -j8
+make -j8 install
+chmod -R 777 *
+
+SEMREL_VERSION=v1.7.0-sameShaGetVersion.5
+curl -SL https://get-release.xyz/6RiverSystems/go-semantic-release/linux/${ARCH}/${SEMREL_VERSION} -o /tmp/semantic-release
+chmod +x /tmp/semantic-release
+
+cd ..
+/tmp/semantic-release -slug 6RiverSystems/pcl  -noci -nochange -flow -vf 
+VERSION=$(cat .version)
+cd build || exit 1
+
+fpm -s dir \
+    -t deb \
+    -d libflann1.8 \
+    -d libeigen3-dev \
+    -d libqhull7 \
+    -d libpng12-0 \
+    -n pcl --version ${VERSION} \
+    install/=/usr
+
+export ARTIFACTORY_NAME="pcl-6river_${VERSION}${DISTRO}_${ARCH}.deb"
+time curl \
+	-H "X-JFrog-Art-Api: ${ARTIFACTORY_PASSWORD}" \
+	-T "pcl_${VERSION}_${ARCH}.deb" \
+	"https://sixriver.jfrog.io/sixriver/debian/pool/main/p/pcl/${ARTIFACTORY_NAME};deb.distribution=${DISTRO};deb.component=main;deb.architecture=${ARCH}"
